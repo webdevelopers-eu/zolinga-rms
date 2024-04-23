0d26faa60211d52acf3a17d1f646495a7babd82e
Danny <danny@webdevelopers.eu>
2024-04-23 19:03:53 +0200

    Update CmsIntegration class to add login/logout functionality and styles

--- src/CmsIntegration.php
+++ src/CmsIntegration.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 namespace Zolinga\Rms;
-use Zolinga\System\Events\{ListenerInterface,ContentEvent};
+
+use Zolinga\System\Events\{ListenerInterface, ContentEvent};
 
 /**
  * Cms integration class.
@@ -11,32 +12,34 @@ use Zolinga\System\Events\{ListenerInterface,ContentEvent};
  * @author Daniel Sevcik <danny@zolinga.net>
  * @date 2024-04-19
  */
-class CmsIntegration implements ListenerInterface {
+class CmsIntegration implements ListenerInterface
+{
 
     public function onContent(ContentEvent $event): void
     {
         global $api;
+
+        $headNode = $event->xpath->evaluate('/html/head')?->item(0);
+        if (!$headNode) { // no html content?
+            return;
+        }
 
         // Do something with the content event.
         $htmlClass = $event->content->documentElement->getAttribute('class');
         $htmlClass .= ' ' . ($api->user->id ? 'rms-logged-in' : 'rms-logged-out');
         $event->content->documentElement->setAttribute('class', trim($htmlClass));
 
-        $res = $event->xpath->query('/html/head');
-        if ($res && $res->length > 0) {
-            $headNode = $res->item(0);
+        // Watch for login/logout cookies
+        $script = $event->content->createElement('script');
+        $headNode->appendChild($script);
+        $script->setAttribute('src', '/dist/zolinga-rms/rms.js');
+        $script->setAttribute('type', 'module');
 
-            // Watch for login/logout cookies
-            $script = $event->content->createElement('script');
-            $headNode->appendChild($script);
-            $script->setAttribute('src', '/dist/zolinga-rms/rms.js');
-            $script->setAttribute('type', 'module');
-
-            // Add styles
-            $style = $event->content->createElement('style');
-            $headNode->appendChild($style);
-            $style->setAttribute('type', 'text/css');
-            $style->appendChild($event->content->createTextNode(<<<CSS
+        // Add styles
+        $style = $event->content->createElement('style');
+        $headNode->appendChild($style);
+        $style->setAttribute('type', 'text/css');
+        $style->appendChild($event->content->createTextNode(<<<CSS
                 html.rms-logged-out .for-users,
                 html.rms-logged-in .for-guests {
                     display: none !important;
@@ -45,6 +48,5 @@ class CmsIntegration implements ListenerInterface {
                     opacity: 0 !important;
                 }
                 CSS));
-        }
     }
 }
