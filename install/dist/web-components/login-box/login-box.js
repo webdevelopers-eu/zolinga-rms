10c7a2929936d0a54a200f765cc2f3d58d84c5ee
Danny <danny@webdevelopers.eu>
2024-05-22 21:49:38 +0200

    Update import paths for api.js and web-component.js in multiple files

--- install/dist/web-components/login-box/login-box.js
+++ install/dist/web-components/login-box/login-box.js
@@ -1,4 +1,4 @@
-import WebComponent from '/dist/system/js/web-component.js';
+import WebComponent from '/dist/zolinga-intl/js/web-component-intl.js';
 import api from '/dist/system/js/api.js';
 
 export default class LoginBox extends WebComponent {
